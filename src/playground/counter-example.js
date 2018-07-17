d140b5ce763418740e80e2c4bb083feb2b4d00e2
antona <antona@walla.net.il>
2018-07-17 12:21:18 +0300

    Ended section 5, moved to section 6

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -7,8 +7,22 @@ class Counter extends React.Component{
         this.handleReset    = this.handleReset.bind(this);
 
         this.state = {
-            count: props.count,
+            count: 0,
         };
+    };
+
+    componentDidMount(){
+        const count = parseInt(localStorage.getItem('count'));
+
+        if(!isNaN(count)){
+            this.setState(() => ({ count: count}));
+        }
+    };
+
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.count !== this.state.count){
+            localStorage.setItem('count', this.state.count);
+        }
     };
 
     handleAddOne(){
@@ -44,8 +58,6 @@ class Counter extends React.Component{
         );
     };
 };
-Counter.defaultProps = {
-    count: 0
-};
+
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+console.log('utils.js is runnng');
+
+export const square     = (num) => num * num;
+export const add        = (a,b) => a + b;
+const subtract          = (a,b) => a - b;
+
+
+export default subtract;
